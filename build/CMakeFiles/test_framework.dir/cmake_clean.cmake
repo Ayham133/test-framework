bfbd9578e760a46610dbf8f44bf9a3da8d1b07bb
Ayham133 <ayhamatallah52@gmail.com>
2026-07-27 11:09:03 +0300

    first commit

---
+++ build/CMakeFiles/test_framework.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_framework.dir/main.c.o"
+  "CMakeFiles/test_framework.dir/main.c.o.d"
+  "test_framework"
+  "test_framework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
