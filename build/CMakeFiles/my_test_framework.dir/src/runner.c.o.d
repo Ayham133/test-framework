5ebfb43f386789f07d35772dded84ddcd449b7af
Ayham133 <ayhamatallah52@gmail.com>
2026-08-05 13:24:00 +0300

    Ignore build/ dir

--- build/CMakeFiles/my_test_framework.dir/src/runner.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/my_test_framework.dir/src/runner.c.o: \
- /home/ayham/Desktop/EveryThing/C/test-framework/src/runner.c \
- /usr/include/stdc-predef.h \
- /home/ayham/Desktop/EveryThing/C/test-framework/src/../include/test.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdbool.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
- /home/ayham/Desktop/EveryThing/C/test-framework/src/registry.h \
- /home/ayham/Desktop/EveryThing/C/test-framework/src/test_internal.h \
- /home/ayham/Desktop/EveryThing/C/test-framework/src/reporter.h
